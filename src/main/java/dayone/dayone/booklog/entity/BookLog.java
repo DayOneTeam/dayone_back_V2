6a91cd4f7e49bece129028763bce732e1281eeb1
seokhwan-an <asok369258@naver.com>
2025-01-06 16:59:14 +0900

    feat : BookLog 엔티티 추가

---
+++ src/main/java/dayone/dayone/booklog/entity/BookLog.java
@@ -0,0 +1,36 @@
+package dayone.dayone.booklog.entity;
+
+import dayone.dayone.booklog.entity.value.Comment;
+import dayone.dayone.booklog.entity.value.Passage;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class BookLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Passage passage;
+
+    @Embedded
+    private Comment comment;
+
+    // TODO: 추후에 BOOK 객체를 참조하는 것 고려하기
+    private Long bookId;
+
+    // TODO : 추후에 USER 객체를 참조하는 것 고려하기
+    private Long userId;
+
+    private int likeCount;
+}
