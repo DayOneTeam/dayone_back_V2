a18068a3bd912788e13e201aab1444695752dda0
seokhwan-an <asok369258@naver.com>
2025-01-07 13:48:21 +0900

    feat : book 엔티티 author 필드 추가

--- src/main/java/dayone/dayone/book/exception/BookErrorCode.java
+++ src/main/java/dayone/dayone/book/exception/BookErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 public enum BookErrorCode implements ErrorCode {
 
     TITLE_BLANK_AND_NULL(HttpStatus.BAD_REQUEST, 1001, "책 제목은 null 이어거나 빈 값일 수 없습니다"),
-    TITLE_LENGTH_OVER(HttpStatus.BAD_REQUEST, 1001, "제목은 100자를 넘길 수 없습니다.");
+    TITLE_LENGTH_OVER(HttpStatus.BAD_REQUEST, 1002, "책 제목은 100자를 넘길 수 없습니다."),
+    AUTHOR_BLACK_AND_NULL(HttpStatus.BAD_REQUEST, 1003, "책 작가명은 null 이어거나 빈 값일 수 없습니다"),
+    AUTHOR_LENGTH_OVER(HttpStatus.BAD_REQUEST, 1004, "책 작가명은 100자를 넘길 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final int code;
