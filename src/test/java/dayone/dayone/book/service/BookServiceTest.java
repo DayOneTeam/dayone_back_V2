d02dcad0975a125b840ca63e20e1acbe6c0afc79
seokhwan-an <asok369258@naver.com>
2025-01-08 17:31:43 +0900

    feat : 책 생성 api 테스트 코드 추가

--- src/test/java/dayone/dayone/book/service/BookServiceTest.java
+++ src/test/java/dayone/dayone/book/service/BookServiceTest.java
@@ -1,7 +1,11 @@
 package dayone.dayone.book.service;
 
+import dayone.dayone.book.entity.Book;
+import dayone.dayone.book.entity.repository.BookRepository;
+import dayone.dayone.book.service.dto.BookCreateRequest;
 import dayone.dayone.book.service.dto.BookSearchResponse;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +23,15 @@ class BookServiceTest {
     private ExternalBookSearch externalBookSearch;
 
     @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
     private BookService bookService;
+
+    @AfterEach
+    void cleanUp() {
+        bookRepository.deleteAll();
+    }
 
     @DisplayName("책 검색에 대한 결과를 반환한다.")
     @Test
@@ -47,6 +59,26 @@ class BookServiceTest {
             softly.assertThat(result.get(0).publisher()).isEqualTo(searchResult.get(0).publisher());
             softly.assertThat(result.get(0).pubdate()).isEqualTo(searchResult.get(0).pubdate());
             softly.assertThat(result.get(0).isbn()).isEqualTo(searchResult.get(0).isbn());
+        });
+    }
+
+    @DisplayName("책을 생성한다.")
+    @Test
+    void createBook() {
+        // given
+        final BookCreateRequest bookCreateRequest = new BookCreateRequest("제목", "저자", "판매처", "이미지", "ISBN");
+
+        // when
+        final Long savedBookId = bookService.create(bookCreateRequest);
+
+        // then
+        final Book book = bookRepository.findById(savedBookId).get();
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(book.getTitle()).isEqualTo(bookCreateRequest.title());
+            softly.assertThat(book.getAuthor()).isEqualTo(bookCreateRequest.author());
+            softly.assertThat(book.getPublisher()).isEqualTo(bookCreateRequest.publisher());
+            softly.assertThat(book.getThumbnail()).isEqualTo(bookCreateRequest.thumbnail());
+            softly.assertThat(book.getIsbn()).isEqualTo(bookCreateRequest.isbn());
         });
     }
 }
