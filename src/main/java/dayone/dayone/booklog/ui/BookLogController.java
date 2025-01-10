fd25076794e022ad331f746f1444a6b65ed68b7e
seokhwan-an <asok369258@naver.com>
2025-01-10 18:03:10 +0900

    feat : BookLog 상세조회 api 추가

--- src/main/java/dayone/dayone/booklog/ui/BookLogController.java
+++ src/main/java/dayone/dayone/booklog/ui/BookLogController.java
@@ -2,11 +2,13 @@ package dayone.dayone.booklog.ui;
 
 import dayone.dayone.booklog.service.BookLogService;
 import dayone.dayone.booklog.service.dto.BookLogCreateRequest;
+import dayone.dayone.booklog.service.dto.BookLogDetailResponse;
 import dayone.dayone.booklog.service.dto.BookLogListResponse;
 import dayone.dayone.global.response.CommonResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,11 @@ public class BookLogController {
     public CommonResponseDto<BookLogListResponse> getBookLogs(@RequestParam(value = "cursor", defaultValue = "-1") String cursor) {
         final BookLogListResponse response = bookLogService.getAllBookLogs(Long.parseLong(cursor));
         return CommonResponseDto.forSuccess(1, "BookLog 조회 성공", response);
+    }
+
+    @GetMapping("/{id}")
+    public CommonResponseDto<BookLogDetailResponse> getBookLogById(@PathVariable("id") Long bookLogId) {
+        final BookLogDetailResponse response = bookLogService.getBookLogById(bookLogId);
+        return CommonResponseDto.forSuccess(1, "BookLog 상세 조회 성공", response);
     }
 }
