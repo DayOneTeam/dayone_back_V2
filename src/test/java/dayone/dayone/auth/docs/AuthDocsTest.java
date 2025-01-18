cef53b7aaa5720b7c11bd6917ba947f22175f968
seokhwan-an <asok369258@naver.com>
2025-01-18 14:31:32 +0900

    feat : 인증되지 않는 유저의 로그아웃 요청 문서화 추가

--- src/test/java/dayone/dayone/auth/docs/AuthDocsTest.java
+++ src/test/java/dayone/dayone/auth/docs/AuthDocsTest.java
@@ -5,6 +5,7 @@ import dayone.dayone.auth.application.dto.TokenInfo;
 import dayone.dayone.support.DocsTest;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -29,66 +30,100 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class AuthDocsTest extends DocsTest {
 
-    @DisplayName("로그인 시 accessToken과 refreshToken을 반환한다.")
-    @Test
-    void successLogin() throws Exception {
-        // given
-        final LoginRequest request = new LoginRequest("이메일", "비밀번호");
-        TokenInfo response = new TokenInfo("accessToken", "refreshToken");
-        given(authService.login(any())).willReturn(response);
+    @DisplayName("로그인 요청")
+    @Nested
+    class Login {
+        @DisplayName("로그인 성공시 accessToken과 refreshToken을 반환한다.")
+        @Test
+        void successLogin() throws Exception {
+            // given
+            final LoginRequest request = new LoginRequest("이메일", "비밀번호");
+            TokenInfo response = new TokenInfo("accessToken", "refreshToken");
+            given(authService.login(any())).willReturn(response);
 
-        final Cookie cookie = new Cookie("refreshToken", "refreshToken value");
-        given(cookieProvider.createAuthCookie(any(), any())).willReturn(cookie);
+            final Cookie cookie = new Cookie("refreshToken", "refreshToken value");
+            given(cookieProvider.createAuthCookie(any(), any())).willReturn(cookie);
 
-        // when
-        final ResultActions result = mockMvc.perform(post("/api/v1/auth/login")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(request)));
+            // when
+            final ResultActions result = mockMvc.perform(post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)));
 
-        // then
-        result.andExpect(status().isOk())
-            .andDo(document("success-login",
-                preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()),
-                requestFields(
-                    fieldWithPath("email").description("이메일"),
-                    fieldWithPath("password").description("비밀번호")
-                ),
-                responseHeaders(
-                    headerWithName(HttpHeaders.SET_COOKIE).description("refreshToken 정보")
-                ),
-                responseFields(
-                    fieldWithPath("code").type(JsonFieldType.NUMBER).description("성공 코드 ex) 1"),
-                    fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메세지 ex) 로그인 성공"),
-                    fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("accessToken")
-                )
-            ));
+            // then
+            result.andExpect(status().isOk())
+                .andDo(document("success-login",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    requestFields(
+                        fieldWithPath("email").description("이메일"),
+                        fieldWithPath("password").description("비밀번호")
+                    ),
+                    responseHeaders(
+                        headerWithName(HttpHeaders.SET_COOKIE).description("refreshToken 정보")
+                    ),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("성공 코드 ex) 1"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메세지 ex) 로그인 성공"),
+                        fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("accessToken")
+                    )
+                ));
+        }
     }
 
-    @DisplayName("로그 아웃 시 refreshToken 쿠키를 삭제한다.")
-    @Test
-    void successLogout() throws Exception {
-        // given
-        final Cookie deletedCookie = new Cookie("refreshToken", null);
-        given(cookieProvider.deleteCookie(any(), any()))
-            .willReturn(deletedCookie);
-        successAuth();
+    @DisplayName("로그아웃 요청")
+    @Nested
+    class Logout {
+        @DisplayName("로그 아웃 시 refreshToken 쿠키를 삭제한다.")
+        @Test
+        void successLogout() throws Exception {
+            // given
+            final Cookie deletedCookie = new Cookie("refreshToken", null);
+            given(cookieProvider.deleteCookie(any(), any()))
+                .willReturn(deletedCookie);
+            successAuth();
 
-        // when
-        final ResultActions result = mockMvc.perform(delete("/api/v1/auth/logout")
-            .header(HttpHeaders.AUTHORIZATION, "Bearer accessToken"));
+            // when
+            final ResultActions result = mockMvc.perform(delete("/api/v1/auth/logout")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer accessToken"));
 
-        // then
-        result.andExpect(status().isNoContent())
-            .andDo(document("success-logout",
-                preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()),
-                requestHeaders(
-                    headerWithName(HttpHeaders.AUTHORIZATION).description("accessToken")
-                ),
-                responseHeaders(
-                    headerWithName(HttpHeaders.SET_COOKIE).description("유효기간이 0인 refreshToken 쿠키")
-                )
-            ));
+            // then
+            result.andExpect(status().isNoContent())
+                .andDo(document("success-logout",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    requestHeaders(
+                        headerWithName(HttpHeaders.AUTHORIZATION).description("accessToken")
+                    ),
+                    responseHeaders(
+                        headerWithName(HttpHeaders.SET_COOKIE).description("유효기간이 0인 refreshToken 쿠키")
+                    )
+                ));
+        }
+
+        @DisplayName("비로그인 사용자가 로그아웃 시도 시 401 예외를 발생한다.")
+        @Test
+        void failLogoutWithUnAuthenticatedUser() throws Exception {
+            // given
+            failAuth();
+
+            // when
+            final ResultActions result = mockMvc.perform(delete("/api/v1/auth/logout")
+                .header(HttpHeaders.AUTHORIZATION, "비어있거나 혹은 존재하지 않는 UserToken 정보"));
+
+            // then
+            result.andExpect(status().isUnauthorized())
+                .andDo(document("fail-logout",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    requestHeaders(
+                        headerWithName(HttpHeaders.AUTHORIZATION).description("비어 있거나 존재하지 않는 User의 Token 정보")
+                    ),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("실패 코드 ex) 4003"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메세지 ex) 로그인 되지 않은 유저입니다."),
+                        fieldWithPath("data").type(null).description("null")
+                    )
+                ));
+        }
     }
 }
