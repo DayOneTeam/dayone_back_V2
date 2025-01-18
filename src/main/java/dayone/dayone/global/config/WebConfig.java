a440cd8db4c10a776e74b0ad377e5a8292995ce4
seokhwan-an <asok369258@naver.com>
2025-01-18 16:55:25 +0900

    feat : 인증된 유저 id 정보를 어노테이션 기반으로 불러오는 resolver 추가

--- src/main/java/dayone/dayone/global/config/WebConfig.java
+++ src/main/java/dayone/dayone/global/config/WebConfig.java
@@ -1,20 +1,30 @@
 package dayone.dayone.global.config;
 
+import dayone.dayone.auth.ui.argumentresolver.AuthArgumentResolver;
 import dayone.dayone.auth.ui.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
+    private final AuthArgumentResolver authArgumentResolver;
 
     @Override
     public void addInterceptors(final org.springframework.web.servlet.config.annotation.InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
             .addPathPatterns("/api/**")
             .excludePathPatterns("/api/v1/auth/login");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
     }
 }
