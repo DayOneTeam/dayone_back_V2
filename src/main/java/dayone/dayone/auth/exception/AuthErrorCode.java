156a6d2d63e6c93a83d9b357ed6d467e689bf390
seokhwan-an <asok369258@naver.com>
2025-01-28 22:48:11 +0900

    refactor : 중복 로그인을 허용하지 않는다

--- src/main/java/dayone/dayone/auth/exception/AuthErrorCode.java
+++ src/main/java/dayone/dayone/auth/exception/AuthErrorCode.java
@@ -8,7 +8,8 @@ public enum AuthErrorCode implements ErrorCode {
     FAIL_LOGIN(HttpStatus.BAD_REQUEST, 4001, "이메일 혹은 비밀번호가 틀렸습니다."),
     HAVE_NOT_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, 4002, "refresh 토큰이 존재하지 않습니다."),
     NOT_LOGIN_USER(HttpStatus.UNAUTHORIZED, 4003, "로그인되지 않은 유저입니다."),
-    HAVE_WRONG_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, 4004, "잘못된 refresh 토큰을 가지고 있습니다.");
+    HAVE_WRONG_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, 4004, "잘못된 refresh 토큰을 가지고 있습니다."),
+    ALREADY_LOGIN(HttpStatus.BAD_REQUEST, 4005, "이미 로그인되어 있습니다.");
 
     private final HttpStatus httpStatus;
     private final int code;
