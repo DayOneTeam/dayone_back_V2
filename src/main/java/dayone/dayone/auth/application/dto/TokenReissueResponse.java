64687d6e8c18efa97da619dac8231805cbc30ddd
seokhwan-an <asok369258@naver.com>
2025-01-30 15:57:37 +0900

    refactor : 응답 메시지 필드 형태 snake case로 변경

--- src/main/java/dayone/dayone/auth/application/dto/TokenReissueResponse.java
+++ src/main/java/dayone/dayone/auth/application/dto/TokenReissueResponse.java
@@ -1,6 +1,9 @@
 package dayone.dayone.auth.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record TokenReissueResponse(
+    @JsonProperty("access_token")
     String accessToken
 ) {
 }
