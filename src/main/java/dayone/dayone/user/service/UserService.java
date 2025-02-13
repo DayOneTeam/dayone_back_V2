e9d87b420a0537b0a5dcc43b5ec3727da110b787
seokhwan-an <asok369258@naver.com>
2025-02-13 15:49:43 +0900

    feat : 유저 정보 조회 api 추가

---
+++ src/main/java/dayone/dayone/user/service/UserService.java
@@ -0,0 +1,23 @@
+package dayone.dayone.user.service;
+
+import dayone.dayone.user.entity.User;
+import dayone.dayone.user.entity.repository.UserRepository;
+import dayone.dayone.user.exception.UserErrorCode;
+import dayone.dayone.user.exception.UserException;
+import dayone.dayone.user.service.dto.UserInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserInfoResponse getUserInfo(final Long userId) {
+        final User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(UserErrorCode.NOT_EXIST_USER));
+
+        return UserInfoResponse.from(user);
+    }
+}
