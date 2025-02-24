ee42cfd9f747da23640a72eb39c124eed15af7a6
seokhwan-an <asok369258@naver.com>
2025-02-24 22:22:20 +0900

    feat : 유저 프로필 이미지 수정 api 추가

--- src/main/java/dayone/dayone/user/service/UserService.java
+++ src/main/java/dayone/dayone/user/service/UserService.java
@@ -1,5 +1,6 @@
 package dayone.dayone.user.service;
 
+import dayone.dayone.global.service.FileService;
 import dayone.dayone.user.entity.User;
 import dayone.dayone.user.entity.repository.UserRepository;
 import dayone.dayone.user.exception.UserErrorCode;
@@ -8,6 +9,9 @@ import dayone.dayone.user.service.dto.UserInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -15,11 +19,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final FileService filesService;
 
     public UserInfoResponse getUserInfo(final Long userId) {
         final User user = userRepository.findById(userId)
             .orElseThrow(() -> new UserException(UserErrorCode.NOT_EXIST_USER));
 
         return UserInfoResponse.from(user);
+    }
+
+    @Transactional
+    public void updateUserProfileImage(final Long userId, final MultipartFile file) throws IOException {
+        final User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(UserErrorCode.NOT_EXIST_USER));
+
+        final String newProfileImage = filesService.uploadFile(file);
+        user.updateProfileImage(newProfileImage);
     }
 }
--- src/main/java/dayone/dayone/user/ui/UserController.java
+++ src/main/java/dayone/dayone/user/ui/UserController.java
@@ -5,9 +5,16 @@ import dayone.dayone.global.response.CommonResponseDto;
 import dayone.dayone.user.service.UserService;
 import dayone.dayone.user.service.dto.UserInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
@@ -20,5 +27,11 @@ public class UserController {
     public CommonResponseDto<UserInfoResponse> getUserInfo(@AuthUser final Long userId) {
         final UserInfoResponse response = userService.getUserInfo(userId);
         return CommonResponseDto.forSuccess(1, "유저 정보 조회 성공", response);
+    }
+
+    @PatchMapping("/update-profile-image")
+    public ResponseEntity<Void> updateUserProfileImage(@AuthUser final Long userId, @RequestPart("file") final MultipartFile file) throws IOException {
+        userService.updateUserProfileImage(userId, file);
+        return ResponseEntity.noContent().build();
     }
 }
