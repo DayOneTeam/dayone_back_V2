4370163d0a5f2e62d63930a8f5fa410ea72627c4
seokhwan-an <asok369258@naver.com>
2025-04-01 23:23:11 +0900

    refactor : restClient timeout 설정 추가

--- src/main/java/dayone/dayone/global/config/RestClientConfiguration.java
+++ src/main/java/dayone/dayone/global/config/RestClientConfiguration.java
@@ -1,14 +1,28 @@
 package dayone.dayone.global.config;
 
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfiguration {
 
     @Bean
     public RestClient getRestClient() {
-        return RestClient.create();
+        return RestClient.builder()
+            .requestFactory(customRequestFactory())
+            .build();
+    }
+
+    ClientHttpRequestFactory customRequestFactory() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+            .withConnectTimeout(Duration.ofSeconds(5))
+            .withReadTimeout(Duration.ofSeconds(5));
+        return ClientHttpRequestFactories.get(settings);
     }
 }
