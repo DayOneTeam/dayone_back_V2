2f2a530b243711aaf55a54b7751ed7bb0831fbd9
seokhwan-an <asok369258@naver.com>
2025-05-05 13:50:31 +0900

    refactor : error code 동일한 것 수정

--- src/main/java/dayone/dayone/demoday/exception/DemoDayErrorCode.java
+++ src/main/java/dayone/dayone/demoday/exception/DemoDayErrorCode.java
@@ -10,8 +10,8 @@ public enum DemoDayErrorCode implements ErrorCode {
     DEMO_DAY_CAPACITY_UNDER_ZERO(HttpStatus.BAD_REQUEST, 5003, "데모데이 참여 인원은 0명이거나 음수가 될 수 없습니다."),
     NOT_EXIST_DEMO_DAY(HttpStatus.NOT_FOUND, 5004, "데모데이가 존재하지 않습니다."),
     DEMO_DAY_IS_CLOSED(HttpStatus.BAD_REQUEST, 5005, "데모데이가 종료되었습니다."),
-    DEMO_DAY_OWNER_NOT_APPLY_ONESELF(HttpStatus.BAD_REQUEST, 5005, "자신이 등록한 데모데이에는 신청할 수 없습니다."),
-    DEMO_DAY_IS_FULL(HttpStatus.BAD_REQUEST, 5006, "데모데이가 참여 인원이 만료되었습니다.");
+    DEMO_DAY_OWNER_NOT_APPLY_ONESELF(HttpStatus.BAD_REQUEST, 5006, "자신이 등록한 데모데이에는 신청할 수 없습니다."),
+    DEMO_DAY_IS_FULL(HttpStatus.BAD_REQUEST, 5007, "데모데이가 참여 인원이 만료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final int code;
